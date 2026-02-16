9d10948cddc59a22c559f2a7aced8f5c43eb9fca
Jesus <jvicentelopez@safareyes.es>
2026-02-16 13:06:31 +0100

    Camara + CORS

--- smartchef-backend/src/main/java/org/example/smartchef/security/CorsConfig.java
+++ smartchef-backend/src/main/java/org/example/smartchef/security/CorsConfig.java
@@ -1,16 +1,17 @@
 package org.example.smartchef.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
-    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://frontend-smartchef.onrender.com")
+                .allowedOrigins("http://localhost:4200", "https://frontend-smartchef.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
 }
